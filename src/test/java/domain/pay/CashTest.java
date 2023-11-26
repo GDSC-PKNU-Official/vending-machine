11f1bb11f9d168706dde80c5c0583db4dfea1a83
nuyh <hjaehyun25@gmail.com>
2023-11-26 11:53:07 +0900

    feat: 전체 금액권을 내림차순으로 조회하는 기능 구현

--- src/test/java/domain/pay/CashTest.java
+++ src/test/java/domain/pay/CashTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,5 +22,16 @@ class CashTest {
 
         //then
         assertThat(added).isEqualTo(BigDecimal.valueOf(1_000_000));
+    }
+
+    @Test
+    @DisplayName("모든 현금을 금액권이 큰 순서대로 조회할 수 있다")
+    void getByDescending() {
+        //when
+        final List<Cash> allCashes = Cash.getAllByDescending();
+
+        //then
+        assertThat(allCashes.stream().map(Cash::getPrice))
+                .isSortedAccordingTo(Comparator.reverseOrder());
     }
 }
