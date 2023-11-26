7afea282b5e024c88765f546d9b982f39587a379
nuyh <hjaehyun25@gmail.com>
2023-11-26 11:18:36 +0900

    fix: 금액권의 금액 수정

--- src/main/java/domain/pay/Cash.java
+++ src/main/java/domain/pay/Cash.java
@@ -3,9 +3,9 @@ package domain.pay;
 import java.math.BigDecimal;
 
 public enum Cash {
-    FIFTY_THOUSAND(BigDecimal.valueOf(50_000)),
-    TEN_THOUSAND(BigDecimal.valueOf(10_000)),
-    ONE_THOUSAND(BigDecimal.valueOf(1_000)),
+    FIFTY_THOUSAND(BigDecimal.valueOf(500_000)),
+    TEN_THOUSAND(BigDecimal.valueOf(100_000)),
+    ONE_THOUSAND(BigDecimal.valueOf(10_000)),
     FIVE_HUNDRED(BigDecimal.valueOf(500)),
     ONE_HUNDRED(BigDecimal.valueOf(100)),;
 
@@ -13,6 +13,10 @@ public enum Cash {
 
     Cash(final BigDecimal price) {
         this.price = price;
+    }
+
+    public BigDecimal add(final Cash other) {
+        return price.add(other.price);
     }
 
     public BigDecimal getPrice() {
