52ba97d7aff007716fe76f7c96f55bceedd62b29
mimpie <yesl3715@naver.com>
2023-11-26 16:40:10 +0900

    거스름돈 최적화 예외처리

--- vendingMachine.js
+++ vendingMachine.js
@@ -1,4 +1,6 @@
-const readlineSync = require('readline-sync');  
+const readlineSync = require('readline-sync');
+
+const CURRENCY_DENOMINATIONS = [50000, 10000, 5000, 1000, 500, 100];
 
 const coldDrinks = {
   '1': { name: '스프라이트', price: 1500 },
@@ -20,26 +22,44 @@ function insertCash(totalAmount) {
 
   const choice = readlineSync.question('사용자 입력 > ');
 
-  if (choice === '0') { 
+  const inputValidation = () => {
+    !/^\d+$/.test(choice)
+  }
+  // 입력값이 숫자가 아니거나, 지원하는 지폐 및 동전 단위에 해당하지 않으면 재입력 요청
+  if (choice === '0') {
     return 0;
+  } else if (inputValidation() || !CURRENCY_DENOMINATIONS[parseInt(choice, 10) - 1]) {
+    console.log('올바르지 않은 입력입니다. 다시 시도해주세요.');
+    return insertCash(totalAmount);
+    
   } else {
-    const denomination = [50000, 10000, 5000, 1000, 500, 100];
-    const selectedDenomination = denomination[parseInt(choice, 10) - 1];
-
+    const selectedDenomination = CURRENCY_DENOMINATIONS[parseInt(choice, 10) - 1];
     return totalAmount + selectedDenomination;
   }
 }
 
 function calculateChange(amount, totalPrice) {
-    return amount - totalPrice;
-  }
+  let remainingChange = amount - totalPrice;
+  const changeDenominations = {};
+
+  // 최소 지폐와 동전 개수로 거스름돈 계산
+  CURRENCY_DENOMINATIONS.forEach(denomination => {
+    const count = Math.floor(remainingChange / denomination);
+    if (count > 0) {
+      changeDenominations[denomination] = count;
+      remainingChange -= count * denomination;
+    }
+  });
+
+  return changeDenominations;
+}
 
 function calculateVAT(price) {
-    return price * 0.1;
-  }
+  return price * 0.1;
+}
 
 function printOrderDetails(drinkName, paymentType, totalPrice, change) {
-  console.log(`이용해주셔서 감사합니다.\n[주문 음료] \n${drinkName}\n[투입 금액]\n${totalPrice}원\n[잔돈] \n${change}원`);
+  console.log(`이용해주셔서 감사합니다.\n[주문 음료] \n${drinkName}\n[투입 금액]\n${totalPrice}원\n[잔돈] \n${JSON.stringify(change)}`);
 }
 
 function runVendingMachine() {
@@ -56,6 +76,7 @@ function runVendingMachine() {
       console.log(`[${key}] ${coldDrinks[key].name} : ${coldDrinks[key].price}원`);
     }
     const coldDrinkChoice = readlineSync.question('사용자 입력 > ');
+
     selectedDrink = coldDrinks[coldDrinkChoice];
   } else if (drinkType === '2') {
     console.log('------------------------------\n[따뜻한 음료]');
@@ -63,14 +84,19 @@ function runVendingMachine() {
       console.log(`[${key}] ${hotDrinks[key].name} : ${hotDrinks[key].price}원`);
     }
     const hotDrinkChoice = readlineSync.question('사용자 입력 > ');
+
+
     selectedDrink = hotDrinks[hotDrinkChoice];
+  } else {
+    console.log('올바르지 않은 입력입니다. 다시 시도해주세요.');
+    return runVendingMachine();
   }
 
   console.log('------------------------------\n[결제 방식 선택]\n[1] 현금\n[2] 카드 (부가세 10% 적용)');
   const paymentType = readlineSync.question('사용자 입력 > ');
 
   let totalPrice;
-  if (paymentType === '1') {
+  if (paymentType === '1') { 
     totalPrice = selectedDrink.price;
     let totalAmount = 0;
 
@@ -79,11 +105,11 @@ function runVendingMachine() {
     }
 
     const change = calculateChange(totalAmount, totalPrice);
-    printOrderDetails(selectedDrink.name, '현금', totalPrice, change);
+    printOrderDetails(selectedDrink.name, '1', totalPrice, change);
   } else if (paymentType === '2') {
     const vat = calculateVAT(selectedDrink.price);
     totalPrice = selectedDrink.price + vat;
-    printOrderDetails(selectedDrink.name, '카드', totalPrice, 0);
+    printOrderDetails(selectedDrink.name, '2', totalPrice, {});
   }
 }
 
